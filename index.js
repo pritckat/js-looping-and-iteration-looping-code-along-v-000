1ce98a95a94cf744444403d368fc21eb22c0c4e4
Katrina Pritchard <pritckat@gmail.com>
2018-10-11 23:31:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ function printBadges(employeeNames) {
   for (let i = 0; i < employeeNames.length; i++) {
     console.log(`Welcome ${employeeNames[i]}! You are employee #${i}.`)
   }
+  return employeeNames
 }

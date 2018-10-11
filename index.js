41a24c3c92d03f7ad2a7ba27b97be6b38063a5d1
Katrina Pritchard <pritckat@gmail.com>
2018-10-11 23:41:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function printBadges(employeeNames) {
+  for (let i = 0; i < employeeNames.length; i++) {
+    console.log(`Welcome ${employeeNames[i]}! You are employee #${i + 1}.`)
+  }
+  return employeeNames
+}
+
+function tailsNeverFails() {
+  let flips = 0;
+  while (Math.random() >= 0.5) {
+    flips++;
+  }
+  return `You got ${flips} tails in a row!`;
+}
